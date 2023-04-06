8765d3ad22a09423fdad6901c8afbf29fabe49ee
Roman Volosatovs <rvolosatovs@riseup.net>
2023-04-06 12:08:46 +0200

    feat: add `nix` integration
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,88 +1,186 @@
 {
+  inputs.nix-log.inputs.nixify.follows = "nixify";
+  inputs.nix-log.inputs.nixlib.follows = "nixlib";
+  inputs.nix-log.url = github:rvolosatovs/nix-log;
+  inputs.nixify.inputs.nix-log.follows = "nix-log";
   inputs.nixify.inputs.nixlib.follows = "nixlib";
   inputs.nixify.url = github:rvolosatovs/nixify;
   inputs.nixlib.url = github:nix-community/nixpkgs.lib;
 
   outputs = {
     self,
+    nix-log,
     nixify,
     nixlib,
     ...
   }:
     with nixlib.lib;
     with builtins;
+    with nix-log.lib;
     with nixify.lib; let
-      lib.depit = {
+      lib.tar = {
+        id,
         lock,
         manifest,
         pkgs,
+        outputHashAlgo ? "sha512",
       }: let
-        system = pkgs.buildPlatform.system;
-        lock' = mapAttrs (
-          id: {sha512, ...}:
-            pkgs.stdenv.mkDerivation {
-              name = "depit-dep-${id}.tar";
-              builder = pkgs.writeShellScript "depit-tar" ''
-                ${self.packages.${system}.depit}/bin/depit --lock ${lock} --manifest ${manifest} tar ${id} --output $out
-              '';
+        depit = "${self.packages.${pkgs.buildPlatform.system}.depit}/bin/depit";
 
-              preferLocalBuild = true;
-
-              outputHashAlgo = "sha512";
-              outputHash = sha512;
-              outputType = "flat";
-            }
-        ) (readTOML lock);
+        outputHash = (readTOML lock).${id}.${outputHashAlgo};
       in
+        trace' "depit.lib.tar" {
+          inherit
+            depit
+            id
+            lock
+            manifest
+            outputHash
+            outputHashAlgo
+            ;
+        }
         pkgs.stdenv.mkDerivation {
-          name = "depit-deps";
+          inherit
+            outputHash
+            outputHashAlgo
+            ;
 
-          dontUnpack = true;
-          installPhase =
-            ''
-              mkdir -p $out
-            ''
-            + concatLines (attrValues (
-              mapAttrs (
-                id: tar: ''
-                  unpackFile ${tar}
-                  mv wit $out/${id}
-                ''
-              )
-              lock'
-            ));
+          name = "depit-dep-${id}.tar";
+          builder = pkgs.writeShellScript "depit-tar" ''
+            ${depit} --lock ${lock} --manifest ${manifest} tar ${id} --output $out
+          '';
+
+          preferLocalBuild = true;
+
+          outputType = "flat";
         };
+
+      lib.lock = {
+        lock,
+        manifest,
+        pkgs,
+      }:
+        trace' "depit.lib.lock" {
+          inherit
+            lock
+            manifest
+            ;
+        }
+        mapAttrs (id: _:
+          pkgs.stdenv.mkDerivation {
+            name = "depit-dep-${id}";
+            src = lib.tar {
+              inherit
+                id
+                lock
+                manifest
+                pkgs
+                ;
+            };
+            installPhase = ''
+              mkdir -p $out
+              mv * $out
+            '';
+            preferLocalBuild = true;
+          })
+        (readTOML lock);
+
+      lib.writeLockScript = {
+        lock,
+        manifest,
+        pkgs,
+        out ? "$out",
+      } @ args: let
+        lock' = lib.lock {
+          inherit
+            lock
+            manifest
+            pkgs
+            ;
+        };
+      in
+        trace' "depit.lib.writeLockScript" {
+          inherit
+            lock
+            manifest
+            out
+            ;
+        }
+        pkgs.writeShellScript "depit-lock" (concatLines (
+          [
+            ''
+              mkdir -p ${out}
+            ''
+          ]
+          ++ (
+            attrValues (
+              mapAttrs (id: dep: ''
+                ln -s ${dep} ${out}/${id}
+              '')
+              lock'
+            )
+          )
+        ));
     in
       rust.mkFlake {
         src = ./.;
 
         name = "depit";
 
-        clippy.allTargets = true;
-        clippy.workspace = true;
+        excludePaths = [
+          ".github"
+          ".gitignore"
+          "flake.lock"
+          "flake.nix"
+          "garnix.yaml"
+          "LICENSE.asl2"
+          "LICENSE.mit"
+          "README.md"
+        ];
+
+        targets.wasm32-wasi = false;
+        targets.x86_64-pc-windows-gnu = false;
+
         test.workspace = true;
 
         doCheck = false;
 
-        buildOverrides = {pkgs, ...} @ args: {
-          doCheck,
+        buildOverrides = {
+          pkgs,
+          pkgsCross ? pkgs,
+          ...
+        } @ args: {
+          depsBuildBuild ? [],
+          doCheck ? false,
           preBuild ? "",
           ...
-        }: let
-          deps.github-build = lib.depit {
-            inherit pkgs;
+        }:
+          with pkgsCross; let
+            lock.github-build = lib.writeLockScript {
+              inherit pkgs;
 
-            lock = ./tests/github-build/wit/deps.lock;
-            manifest = ./tests/github-build/wit/deps.toml;
-          };
-        in
-          optionalAttrs doCheck {
-            preBuild =
-              preBuild
-              + ''
-                ln -s ${deps.github-build} ./tests/github-build/wit/deps
-              '';
-          };
+              lock = ./tests/github-build/wit/deps.lock;
+              manifest = ./tests/github-build/wit/deps.toml;
+              out = "./tests/github-build/wit/deps";
+            };
+            doCheck' = doCheck || args ? pkgsCross; # native `depit` is a dependency of the tests
+          in
+            {
+              doCheck = doCheck';
+
+              depsBuildBuild =
+                depsBuildBuild
+                ++ optionals stdenv.hostPlatform.isDarwin [
+                  libiconv
+                ];
+            }
+            // optionalAttrs doCheck' {
+              preBuild =
+                preBuild
+                + ''
+                  ${lock.github-build}
+                '';
+            };
 
         withChecks = {
           checks,
@@ -90,14 +188,12 @@
           ...
         }:
           checks
-          // {
-            example-github = self.lib.depit {
-              inherit
-                pkgs
-                ;
-              lock = ./examples/github/wit/deps.lock;
-              manifest = ./examples/github/wit/deps.toml;
-            };
+          // self.lib.lock {
+            inherit
+              pkgs
+              ;
+            lock = ./examples/github/wit/deps.lock;
+            manifest = ./examples/github/wit/deps.toml;
           };
       }
       // {
